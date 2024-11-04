74a42bead3ffa5f858f46ab8648c6706c06da205
joseriobo <joseriobo7@gmail.com>
2024-11-04 18:54:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,59 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "A UFO",
+    "My pet rock",
+    "The local squirrel gang",
+    "A time-traveling robot",
+    "My invisible friend",
+    "A group of angry clowns"
+  ];
+
+  let action = [
+    "juggled",
+    "stole",
+    "exploded",
+    "farted on",
+    "performed a magic trick on",
+    "painted"
+  ];
+
+  let what = [
+    "my sandwich",
+    "the neighbor's lawn",
+    "my collection of rubber ducks",
+    "the WiFi router",
+    "my entire life savings",
+    "the last slice of pizza"
+  ];
+
+  let when = [
+    "during the meteor shower",
+    "while I was talking to my goldfish",
+    "before I could finish my cereal",
+    "while riding a unicycle",
+    "after I accidentally summoned a demon",
+    "in the middle of a karaoke contest"
+  ];
+
+  const excuseRandom = listEl => {
+    return listEl[Math.floor(Math.random() * listEl.length)];
+  };
+
+  let excuseGen = document.querySelector("#excuse");
+
+  if (excuseGen) {
+    excuseGen.innerHTML =
+      excuseRandom(who) +
+      " " +
+      excuseRandom(action) +
+      " " +
+      excuseRandom(what) +
+      " " +
+      excuseRandom(when);
+  } else {
+    console.error("Elemento #excuse no encontrado en el DOM");
+  }
 };
